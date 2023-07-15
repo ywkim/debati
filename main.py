e51749fd2760e959aedf209fcc26d9bee302b8ce
Youngwook Kim <youngwook.kim@gmail.com>
2023-07-16 00:35:06 +0900

    feat(bot): add member intents
    - Add `intents.members = True` to enable member intents in the `bot` object.

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import discord
 from discord.ext import commands
 from config import TOKEN
 
-intents = discord.Intents.default()  # 기본적인 모든 intents를 활성화합니다.
+intents = discord.Intents.default()
+intents.members = True
+
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
-bot.load_extension('commands')
+bot.load_extension('events.on_message')
+bot.load_extension('events.on_member_join')
+
 bot.run(TOKEN)
