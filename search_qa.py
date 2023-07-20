46b58407e8e4bbc283dc2712d96c9f2b338259e5
Youngwook Kim <youngwook.kim@gmail.com>
2023-07-20 09:20:08 +0900

    feat: Add new search examples and error handling in search_qa.py

--- search_qa.py
+++ search_qa.py
@@ -1,24 +1,23 @@
 """Tool that calls SerpAPI and QA.
 """
-from pydantic import BaseModel, Field
-from langchain.tools import BaseTool, StructuredTool, Tool, tool
-
-from typing import Optional, Type, Any
+from typing import Any, Optional, Type
 
 from langchain.callbacks.manager import (
     AsyncCallbackManagerForToolRun,
     CallbackManagerForToolRun,
 )
-from serp_loader import SerpAPILoader
-from langchain.indexes import VectorstoreIndexCreator
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
 from langchain.llms import OpenAI
-from langchain.chains.question_answering import load_qa_chain
-import logging
-import sys
+from langchain.tools import BaseTool
+from langchain.tools.base import ToolException
+from openai.error import OpenAIError
+from pydantic import BaseModel, Field
+
+from serp_loader import SerpAPILoader
 
 
-class SearchSchema(BaseModel):
+class SearchQASchema(BaseModel):
     question: str = Field(description="should be a question on Google search results")
     query: str = Field(
         description="should be a Google search query. You can use anything that you would use in a regular Google search."
@@ -28,10 +27,10 @@ class SearchSchema(BaseModel):
 class SearchQA(BaseTool):
     name = "search_qa"
     description = "Useful for when you need to answer questions about current events"
-    args_schema: Type[SearchSchema] = SearchSchema
-    llm: Any = Field()
+    args_schema: Type[SearchQASchema] = SearchQASchema
+    llm: OpenAI = Field()
     serp: Any = Field()
-    embeddings: Any = Field()
+    embeddings: OpenAIEmbeddings = Field()
 
     def _run(
         self,
@@ -43,17 +42,18 @@ class SearchQA(BaseTool):
         print("SearchQA Tool")
         print(f"Question: {question}")
         print(f"Query: {query}")
-        loader = SerpAPILoader(query, self.serp)
-        # chain = load_qa_chain(llm)
-        # chain.run(input_documents=loader.load(), question="LangChain 공식 문서 주소 검색해봐")
-        index = VectorstoreIndexCreator(embedding=self.embeddings).from_loaders(
-            [loader]
-        )
-        # query = "LangChain 공식 문서 link는?"
-        # index.query_with_sources(question=query, llm=llm)
-
-        res = index.query(question=question, llm=self.llm)
-        return res
+        try:
+            loader = SerpAPILoader(query, self.serp)
+            index = VectorstoreIndexCreator(embedding=self.embeddings).from_loaders(
+                [loader]
+            )
+            answer = index.query(question=question, llm=self.llm)
+            return answer
+        except OpenAIError as e:
+            print(e)
+            raise ToolException() from e
+        except IOError as e:
+            raise ToolException() from e
 
     async def _arun(
         self,
@@ -62,4 +62,4 @@ class SearchQA(BaseTool):
         run_manager: Optional[AsyncCallbackManagerForToolRun] = None,
     ) -> str:
         """Use the tool asynchronously."""
-        raise NotImplementedError("search_qa does not support async")
+        return self._run(question, query)
