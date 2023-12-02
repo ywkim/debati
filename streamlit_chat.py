5f5f17fdba3e1a46c28c943f3d25f1764274dfba
Youngwook Kim <youngwook.kim@gmail.com>
2023-12-03 00:26:47 +0900

    feat(streamlit_app): initialization with service account (#51)

--- streamlit_chat.py
+++ streamlit_chat.py
@@ -6,6 +6,7 @@ from typing import Any
 
 import streamlit as st
 from google.cloud import firestore
+from google.oauth2 import service_account
 from langchain.schema import AIMessage, BaseMessage, HumanMessage
 
 from config.app_config import AppConfig, init_chat_model
@@ -22,7 +23,40 @@ class StreamlitAppConfig(AppConfig):
 
     def _load_config_from_streamlit_secrets(self):
         """Loads configuration from Streamlit secrets."""
-        self.config.read_dict(st.secrets)
+        self.config.read_dict(
+            {
+                key: value
+                for key, value in st.secrets.items()
+                if key != "firebase_service_account"
+            }
+        )
+
+    def _initialize_firebase_client(self) -> firestore.Client:
+        """
+        Initializes and returns a Firebase Firestore client using the service account details from Streamlit secrets.
+
+        Returns:
+            firestore.Client: The initialized Firestore client.
+
+        Raises:
+            ValueError: If the required service account details are not provided in Streamlit secrets.
+        """
+        service_account_info = st.secrets.get("firebase_service_account")
+        if not service_account_info:
+            logging.info(
+                "Firebase service account details not found in Streamlit secrets. Using default credentials."
+            )
+            return firestore.Client()
+
+        # Create a service account credential object
+        credentials = service_account.Credentials.from_service_account_info(
+            service_account_info
+        )
+
+        project_id = service_account_info["project_id"]
+
+        # Initialize and return the Firestore client with the credentials
+        return firestore.Client(credentials=credentials, project=project_id)
 
     def load_config_from_firebase(self, companion_id: str) -> None:
         """
@@ -34,7 +68,7 @@ class StreamlitAppConfig(AppConfig):
         Raises:
             FileNotFoundError: If the companion document does not exist in Firebase.
         """
-        db = firestore.Client()
+        db = self._initialize_firebase_client()
         companion_ref = db.collection("Companions").document(companion_id)
         companion = companion_ref.get()
         if not companion.exists:
@@ -80,7 +114,10 @@ def handle_chat_interaction(app_config: StreamlitAppConfig) -> None:
     if "thread_messages" not in st.session_state:
         st.session_state.thread_messages = []
 
-    st.title("Buppy")
+    if "companion_id" in st.session_state:
+        st.title(st.session_state.companion_id)
+    else:
+        st.title("Buppy")
 
     # Display existing chat messages
     display_messages(st.session_state.thread_messages)
